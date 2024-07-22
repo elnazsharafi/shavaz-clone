363299fbf4e73b49272570e19b6dce8443842f15
Elnaz Sharafi <elnaz3386@gmail.com>
2024-07-22 22:00:36 +0330

    feat: get promotion banner from api

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,8 +1,21 @@
+<script>
+export default {
+  data: () => ({
+    banner: null,
+  }),
+  mounted() {
+    fetch('http://localhost:8000/api/promotion-banner')
+        .then(response => response.json())
+        .then(banner => this.banner = banner)
+        .catch(err => console.log(err.message))
+  }
+}
+</script>
 <template>
-  <v-sheet>
+  <v-sheet v-if="banner">
     <a class="fixed-banner d-block" id="top-banner" href="https://shavaz.com/">
       <img
-          src="/images/banner2.jpg"
+          :src="banner.imageUrl"
           class="w-100 h-100" alt="free delivery" width="1400" height="64" title="free delivery">
     </a>
   </v-sheet>
