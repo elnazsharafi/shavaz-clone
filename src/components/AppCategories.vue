a4a1e7ccfba68fd75d968a7a2e5cea2fced7df4d
Elnaz Sharafi <elnaz3386@gmail.com>
2024-07-23 17:55:06 +0330

    get categoryItems from api

--- src/components/AppCategories.vue
+++ src/components/AppCategories.vue
@@ -4,23 +4,14 @@ import CategoryItem from "@/components/CategoryItem.vue";
 export default {
   components: {CategoryItem},
   data: () => ({
-    categoryItems: [
-      {text: "آرایش چشم", imageUrl: "/images/category1.webp"},
-      {text: "دهان و دندان", imageUrl: "/images/category2.webp"},
-      {text: "عطر و ادکلن", imageUrl: "/images/category3.webp"},
-      {text: "رنگ مو", imageUrl: "/images/category4.webp"},
-      {text: "پاک کننده آرایش", imageUrl: "/images/category5.webp"},
-      {text: "مایع ضدعفونی کننده", imageUrl: "/images/category6.webp"},
-      {text: "لاک", imageUrl: "/images/category7.webp"},
-      {text: "لوسیون و روغن بدن", imageUrl: "/images/category8.webp"},
-      {text: "آرایش لب", imageUrl: "/images/category9.webp"},
-      {text: "ماسک مو", imageUrl: "/images/category10.webp"},
-      {text: "ضدتعریق", imageUrl: "/images/category11.webp"},
-      {text: "نواربهداشتی", imageUrl: "/images/category12.webp"},
-      {text: "آرایش صورت", imageUrl: "/images/category13.webp"},
-      {text: "دستمال مرطوب", imageUrl: "/images/category14.webp"},
-    ]
-  })
+    categoryItems: [],
+  }),
+  mounted() {
+    fetch('http://localhost:8000/api/category-items')
+        .then(response => response.json())
+        .then(items => this.categoryItems = items)
+        .catch(err => console.log(err.message))
+  }
 }
 </script>
 
